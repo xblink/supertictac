ca9501d02cd715e9b9d34672620cfde0548ff6df
Jed Eldridge <jedeldridge@gmail.com>
2020-08-08 01:14:05 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# supertictac
+### suepr tic-tac-toe in a console
+
+Just messing around with old things.
+
+## To Run
+> python supertictac.py
---
+++ supertictac/supertictac.py
@@ -0,0 +1,8 @@
+#!/urs/bin/env python3
+
+"""supertictac.py - super-tictactoe console implementation"""
+def main():
+    print("Hello World.")
+
+if __name__ == "__main__":
+    main()
